5825b5c2fc3215e5235936270864b34aa11ad440
Gleb Bespalov <ga.bespalov@gmail.com>
2020-08-25 18:41:24 +1100

    Реализована работа UserServiceImpl через Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,171 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private static EntityManagerFactory entityManagerFactory;
+    private final String tableName = User.class.getAnnotation(Table.class).name();
 
+    public UserDaoHibernateImpl() {
+        try {
+            entityManagerFactory =  Util.getEntityManagerFactory();
+        } catch (Exception ex) {
+            System.err.printf("При попытке соединения с БД возникла ошибка:%n");
+            System.err.println("\t==>" + ex.getMessage());
+            System.exit(1);
+        }
     }
 
 
     @Override
     public void createUsersTable() {
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
 
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.createNativeQuery(
+                    "CREATE TABLE IF NOT EXISTS " + tableName + "(id INT AUTO_INCREMENT PRIMARY KEY, " +
+                            "name VARCHAR(20) NOT NULL, lastname VARCHAR(35) NOT NULL, age TINYINT(3) UNSIGNED);")
+                    .executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
 
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.createNativeQuery("DROP TABLE IF EXISTS " + tableName + ';').executeUpdate();
+            tx.commit();
+        }  catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
 
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.persist(new User(name, lastName, age));
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
 
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.remove(entityManager.getReference(User.class, id));
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
+        List<User> result = Collections.emptyList();
+
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            result = entityManager.createQuery("from User", User.class).getResultList();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        return result;
     }
 
     @Override
     public void cleanUsersTable() {
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
 
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.createQuery("delete from User").executeUpdate();
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.util;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,5 +13,9 @@ public class Util {
                 "jdbc:mysql://localhost:3306/" + dbName,
                 "root",
                 "");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return Persistence.createEntityManagerFactory("jm.task.core.jdbc.jpa");
     }
 }
